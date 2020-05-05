91ada2d2900b0b66976d49d7630fe8495f0538a1
Utkarsh Mehta <umtebiz@gmail.com>
2020-05-06 02:48:36 +0530

    start typing after modal close

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var i = 0;
 var txt = ', I hope, to someone.'; /* The text */
 var speed = 200; /* The speed/duration of the effect in milliseconds */
 var slideIndex = 0;
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
 
 function typeWriter() {
     if (i < txt.length) {
@@ -53,7 +56,6 @@ TxtType.prototype.tick = function () {
 };
 
 window.onload = function () {
-    this.typeWriter();
     var elements = document.getElementsByClassName('typewrite');
     for (var i = 0; i < elements.length; i++) {
         var toRotate = elements[i].getAttribute('data-type');
@@ -83,23 +85,16 @@ function showSlides() {
     setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
 }
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
+    typeWriter()
+    modal.style.display = "none";    
 }
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
+        this.typeWriter();
         modal.style.display = "none";
     }
-} 
+}   
